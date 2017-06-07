a5c3d3cc8011dba0d46e1337a5eb8cb5882964ac
cindy <mendozaic@hotmail.com>
2017-06-07 13:17:15 -0500

    filtra

--- js/utils/filters.js
+++ js/utils/filters.js
@@ -2,7 +2,7 @@
 
 const filterByDistrict = (stations,query) => {
    return stations.filter((x)=>{
-    const ele = x.district.toLowerCase().indexOf(query.toLowerCase()) =! -1;
+    const ele = x.district.indexOf(query) != -1;
     console.log(stations,query);
     return ele;
   });
